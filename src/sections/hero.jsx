7a0719ba265c29ae1ae1aae7ca7517349b76f610
Nassim <enaciri.nassim@gmail.com>
2025-10-16 23:52:06 +0200

    fix: prevent Canvas from overriding other page elements

---
+++ src/sections/hero.jsx
@@ -0,0 +1,30 @@
+import { PerspectiveCamera } from '@react-three/drei'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import HackerRoom from '../components/HackerRoom'
+    
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex-col relative">
+        <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+            <p className='sm:text-3xl text-xl font-medium text-white text-center font-generalsans'>Hi, Im Nassim <span className='waving-hand'> 👋 </span></p>
+            <p className='hero_tag text-gray_gradient'>
+                I build things for the web
+            </p>
+        </div>
+
+        <div className='w-full h-full absolute inset-0' >
+
+            <Canvas className="w-full h-full">
+                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                <HackerRoom />
+            </Canvas>
+
+        </div>
+        
+
+    </section>
+  )
+}
+
+export default Hero
